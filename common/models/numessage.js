132f67bf3d59e50ea90a6826636587648e879006
Maker Su <yuminsutw@yahoo.com.tw>
2014-12-11 15:33:57 +0800

    add remote method updateinsert for numessage and nusite

--- common/models/numessage.js
+++ common/models/numessage.js
@@ -1,3 +1,40 @@
 module.exports = function(Numessage) {
 
+	Numessage.validatesUniquenessOf('url');
+
+	Numessage.updateInsert = function(data, cb) {
+
+		console.log('url='+data.url)
+
+		Numessage.findOne(
+			{
+				where: {
+					url: data.url
+				} 
+			}, 
+			function(err,numessageInstance) { 
+
+				if(numessageInstance && numessageInstance.id){
+					console.log("existed="+numessageInstance.id);
+					data.id=numessageInstance.id
+				}
+
+				Numessage.upsert(data, function(err,obj){
+						//console.log(err)
+						//console.log(obj)
+						cb(err, obj);
+				});
+			
+			}
+		);
+  }
+     
+  Numessage.remoteMethod(
+      'updateInsert', 
+      {
+        accepts: {arg: 'data', type: 'object'},
+        returns: {arg: 'numessage', type: 'object'}
+      }
+  );
+
 };
--- common/models/nusite.js
+++ common/models/nusite.js
@@ -1,3 +1,40 @@
 module.exports = function(Nusite) {
+	Nusite.validatesUniquenessOf('site_id');
+
+	Nusite.updateInsert = function(data, cb) {
+
+		console.log('site_id='+data.site_id)
+
+		Nusite.findOne(
+			{
+				where: {
+					site_id: data.site_id
+				} 
+			}, 
+			function(err,nusiteInstance) { 
+				//console.log(err)
+				//console.log(siteInstance)
+				if(nusiteInstance && nusiteInstance.id){
+					console.log("existed="+nusiteInstance.id);
+					data.id=nusiteInstance.id
+				}
+
+				Nusite.upsert(data, function(err,obj){
+						//console.log(err)
+						//console.log(obj)
+						cb(err, obj);
+				});
+			
+			}
+		);
+  }
+     
+  Nusite.remoteMethod(
+      'updateInsert', 
+      {
+        accepts: {arg: 'data', type: 'object'},
+        returns: {arg: 'nusite', type: 'object'}
+      }
+  );
 
 };
